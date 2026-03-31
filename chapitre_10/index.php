886f89b5928281483a7cdd2da4da6e952b89b03a
Salmaki <Salmakikout1@gmail.com>
2026-03-31 09:15:53 +0100

    ch

---
+++ chapitre_10/index.php
@@ -0,0 +1,20 @@
+<?php
+require 'Database.php';
+require 'User.php';
+
+
+$database = new Database();
+$db = $database->getConnection();
+
+
+$user = new User($db);
+$user->nom = "Alice";
+$user->email = "alice@test.com";
+$user->create();
+
+
+
+$liste = $user->read();
+foreach($liste as $u) {
+    echo $u['nom'] . " - " . $u['email'] . "<br>";
+}
---
+++ chapitre_10/user.php
@@ -0,0 +1,47 @@
+<?php
+class user
+{
+
+    private $conn;
+    private $table = "users";
+
+
+    public $id;
+    public $nom;
+    public $email;
+
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function create()
+    {
+
+        $sql = "INSERT INTO {$this->table} (nom, email) VALUES (:nom, :email)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute(['nom' => $this->nom, 'email' => $this->email]);
+    }
+
+    public function read()
+    {
+
+        $sql = "SELECT * FROM {$this->table}";
+        $stmt = $this->conn->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function update(){
+        $sql ="UPDATE {$this->table} SET nom=:nom, email=: WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt->execute(['nom' => $this->nom, 'email' => $this->email, 'id' => $this->id]);
+    }
+
+    public function delete() {
+            $sql = "DELETE FROM {$this->table} WHERE id=:id";
+            $stmt = $this->conn->prepare($sql);
+            return $stmt->execute(['id' => $this->id]);
+
+    }
+}
